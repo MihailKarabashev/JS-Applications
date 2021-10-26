1110940cba3bb39a33cf9226d14f9adb67be9db7
mihail-karabashev <mihailkarabashev@gmail.com>
2021-10-26 16:18:03 +0300

    up

--- asynchronous-programming/Exercise/06.Blog/app.js
+++ asynchronous-programming/Exercise/06.Blog/app.js
@@ -1,5 +1,68 @@
 function attachEvents() {
-    console.log('TODO...');
+   document.querySelector('#btnLoadPosts').addEventListener('click',fillOptions);
+   document.querySelector('#btnViewPost').addEventListener('click', getPostInfo);
+   let posts = [];
+
+
+   async function fillOptions(){
+    let data = await getData();
+
+    posts = [...Object.values(data)];
+    console.log(posts);
+   
+     let selectElement = document.querySelector('#posts');
+   
+    Object.values(data).forEach(post => {
+       let option = createOption(post);
+       selectElement.appendChild(option);
+    });
+   }
+   
+   async function getPostInfo(e){
+       let select = e.target.previousElementSibling;
+       let id = select.options[select.selectedIndex].value;
+
+       let post = posts.find(currPost => currPost.id === id);
+   
+       let postTitle = document.querySelector('#post-title');
+       postTitle.textContent = post.title;
+
+       let postBody = document.querySelector('#post-body');
+       postBody.textContent = post.body;
+
+       let comments= document.querySelector('#post-comments');
+       
+      let data = await getData();
+   
+       Object.values(data).filter(x=> x.postId === id).forEach(currComment => {
+           let comment = createComment(currComment);
+           comments.appendChild(comment);
+       });
+   }
+
+   async function getData(){
+    let response = await fetch('http://localhost:3030/jsonstore/blog/comments');
+    let data = await response.json();
+
+    return data;
+   }
+   
+   function createComment(comment){
+      let liCommentElement = document.createElement('li');
+      liCommentElement.id = comment.id;
+      liCommentElement.textContent = comment.text;
+
+      return liCommentElement;
+   }
+
+   function createOption(post){
+       let option = document.createElement('option');
+       option.value = post.id;
+       option.textContent = post.title;
+   
+       return option;
+   }
 }
 
 attachEvents();
+
